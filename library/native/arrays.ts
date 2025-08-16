880352ff0075449bbd9936c852d1fb13325ff2bb
Ben Dean <benneb123@gmail.com>
2025-08-16 15:24:59 -0500

    Update arrays.ts

--- library/native/arrays.ts
+++ library/native/arrays.ts
@@ -5,6 +5,7 @@ import {
   Var,
   unify_2,
   makeTerm,
+  def_func,
 } from "pl-runtime";
 
 def_nondet("term_tag_args", 3, function*(
@@ -28,3 +29,22 @@ def_nondet("term_tag_args", 3, function*(
   const term = makeTerm(Tag, Args);
   yield* unify_2(term, Term);
 })
+
+// Note - ret undefined counts as failure i guess
+
+for (const spec of `
+  at3
+  indexOf3 indexOf4
+  lastIndexOf3 lastIndexOf4
+  with4 concat3
+  slice2 slice3 slice4
+  pop2 unshift2
+  join2
+  toReversed2
+  includes3
+`);
+
+for (const spec of `
+  push2 shift2 reverse1
+  fill2 fill3 fill4
+`);
