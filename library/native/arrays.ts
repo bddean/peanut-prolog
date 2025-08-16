934bf114841631a928fa17927308c16446ba74f0
Ben Dean <benneb123@gmail.com>
2025-08-16 10:31:00 -0500

    .

--- library/native/arrays.ts
+++ library/native/arrays.ts
@@ -1,1 +1,30 @@
-export {}
+import {
+  termArgsArray,
+  termTag,
+  def_nondet,
+  Var,
+  unify_2,
+  makeTerm,
+} from "pl-runtime";
+
+def_nondet("term_tag_args", 3, function*(
+  Term,
+  Tag,
+  Args,
+) {
+  if (!(Term instanceof Var)) {
+    const tag = termTag(Term);
+    const args = termArgsArray(Term);
+
+    for (const _ of unify_2(Tag, tag))
+      yield* unify_2(Args, args);
+  }
+  if (Tag instanceof Var || Args instanceof Var) {
+    throw new Error("insufficiently instantiated");
+  }
+  if (! (typeof Tag === "symbol" && Array.isArray(Args)))
+    throw new Error("Bad type");
+
+  const term = makeTerm(Tag, Args);
+  yield* unify_2(term, Term);
+})
