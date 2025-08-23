fccad1c0b0ee957f8b8efed9bd5ad7ddd2ff46bd
Ben Dean <benneb123@gmail.com>
2025-08-22 22:20:22 -0500

    Update directives.pl

--- src/directives.pl
+++ src/directives.pl
@@ -3,11 +3,14 @@
 	compile_current_module/1
 ]).
 :- use_module(goal_ir).
+:- use_module(variables).
+
 
 % By default we assume all unqualified calls live in module `user`.
 compile_current_module(user).
 
 directive_ir(ensure_loaded(Path), import(Path)) :- !.
 
-directive_ir(Goal, ($direct):(IR)) :-
+directive_ir(Goal, ($direct):(Setup, IR)) :-
+  numbervars_allocation(Goal, Setup),
 	goal_ir(block(direct), break($direct), Goal, IR).
