7d3ca7c9c8caa9a599c801027e2bf046137c0444
Ben Dean <benneb123@gmail.com>
2025-08-22 22:17:12 -0500

    Update variables.pl

--- src/variables.pl
+++ src/variables.pl
@@ -1,4 +1,7 @@
- numbervars_declarations(T, allocate_variables(Varnames)) :-
+:- module(variables, [numbervars_allocation/2]).
+
+
+ numbervars_allocation(T, allocate_vars(Varnames)) :-
    numbervars(Clauses, 0, NumVars),
         numlist(0, NumVars, VarNums),
         maplist(var_name_, VarNums, VarNames).
